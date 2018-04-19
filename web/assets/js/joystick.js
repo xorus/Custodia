44132d9ad8e060e1fdf9115d209d0049208ee7fd
franckbattu <franck.battu@outlook.fr>
2018-04-19 19:01:10 +0200

    Ajout socket

--- web/assets/js/joystick.js
+++ web/assets/js/joystick.js
@@ -1,3 +1,7 @@
+let socket = new WebSocket("ws://localhost:8888");
+let joystick = document.querySelector('#static');
+let footer = document.querySelector('#joystick-footer');
+
 let manager = nipplejs.create(
     {
         zone: document.getElementById('static'),
@@ -11,11 +15,28 @@ let manager = nipplejs.create(
     }
 );
 
+// GESTION SOCKET
+
+// Le client est ouvert
+socket.onopen = (event) => {
+    console.log("Je demande une connexion");
+};
+
+// Le serveur a envoyé des messages
+socket.onmessage = (event) => {
+    let data = JSON.parse(event.data);
+    footer.innerHTML = `<p>Position x : ${data.x}, position y : ${data.y}`;
+}
+
+
+
+// GESTION JOYSTICK
 /*
 manager.on('move', function(event, data) {
 	console.log(data);
 });*/
 
-manager.on('dir', function(event, data) {
-	console.log(data);
+manager.on('dir', (event, data) => {
+    console.log(data);
+	socket.send(JSON.stringify(data.position));
 });
